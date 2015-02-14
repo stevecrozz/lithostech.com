81e66d9842d6f288a22ab5080efabed8f48906b1
Stephen Crosby <stevecrozz@gmail.com>
2015-02-14 14:05:49 -0800

    article style enhancements

--- _plugins/ext.rb
+++ _plugins/ext.rb
@@ -1,2 +1,10 @@
 require 'jekyll-assets'
-require 'jekyll-sitemap'
+
+if Jekyll.env == 'production'
+  require 'jekyll-sitemap'
+  require 'octopress-minify-html'
+else
+  require 'pry'
+  Jekyll.configuration['assets']['js_compressor'] = nil
+  Jekyll.configuration['assets']['css_compressor'] = nil
+end
